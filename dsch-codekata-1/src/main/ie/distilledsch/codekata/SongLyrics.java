726ffca4ad8d7c1e9fe4df9baa56d5f0572bbf64
Aoife <29230558+aoifedd@users.noreply.github.com>
2019-08-13 00:18:17 +0100

    Add dsch-codekata-1

---
+++ dsch-codekata-1/src/main/ie/distilledsch/codekata/SongLyrics.java
@@ -0,0 +1,18 @@
+package ie.distilledsch.codekata;
+
+public class SongLyrics{
+
+  static final String FIRST_LINE = "There was an old lady who swallowed ";
+  static final String SECOND_LINE_PART_ONE = "I don't know why she swallowed ";
+  static final String SECOND_LINE_PART_TWO = " - perhaps she'll die!";
+  static final String FINAL_LINE = "...She's dead, of course!";
+
+  static final String SECOND_VERSE = "That wriggled and wiggled and tickled inside her.";
+  static final String THIRD_VERSE = "How absurd to swallow ";
+  static final String FOURTH_VERSE = "Fancy that to swallow ";
+  static final String FIFTH_VERSE = "What a hog, to swallow ";
+  static final String SIXTH_VERSE = "I don't know how she swallowed ";
+
+  static final String LINEBREAK = "\n";
+
+}
